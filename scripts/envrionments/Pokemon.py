c0fe25d3c59940e0d9be55741a26d2e41664be25
beardyface <henryamwilliams@gmail.com>
2023-11-06 16:41:22 +1300

    developing state

--- scripts/envrionments/Pokemon.py
+++ scripts/envrionments/Pokemon.py
@@ -66,6 +66,8 @@ class Pokemon:
                 hide_window=hide_window,
             )
 
+        self.agent_stats = []
+
         with open(self.init_path, "rb") as f:
             self.pyboy.load_state(f)
 
@@ -105,6 +107,8 @@ class Pokemon:
         
         self.run_action_on_emulator(action)
         
+        self.append_agent_stats(action)
+
         new_state = self._generate_state()
         reward = self._calculate_reward(new_state)
         done = self._check_if_done()
@@ -134,14 +138,9 @@ class Pokemon:
         y_pos = self.read_m(0xD361)
         map_n = self.read_m(0xD35E)
         levels = [self.read_m(a) for a in [0xD18C, 0xD1B8, 0xD1E4, 0xD210, 0xD23C, 0xD268]]
-        
-        if self.use_screen_explore:
-            expl = ('frames', self.knn_index.get_current_count())
-        else:
-            expl = ('coord_count', len(self.seen_coords))
-
+    
         self.agent_stats.append({
-            'step': self.step_count, 'x': x_pos, 'y': y_pos, 'map': map_n,
+            'x': x_pos, 'y': y_pos, 'map': map_n,
             'map_location': self.get_map_location(map_n),
             'last_action': action,
             'pcount': self.read_m(0xD163), 
@@ -149,10 +148,9 @@ class Pokemon:
             'levels_sum': sum(levels),
             'ptypes': self.read_party(),
             'hp': self.read_hp_fraction(),
-            expl[0]: expl[1],
-            'deaths': self.died_count, 'badge': self.get_badges(),
-            'event': self.progress_reward['event'], 'healr': self.total_healing_rew
+            'badge': self.get_badges(),
         })
+        print(self.agent_stats)
 
     def update_reward(self):
         # compute reward
@@ -369,3 +367,38 @@ class Pokemon:
             return map_locations[map_idx]
         else:
             return "Unknown Location"
+
+class PoekmonImage(Pokemon):
+    def __init__(self, config: GymEnvironmentConfig, k=3):
+        self.k    = k  # number of frames to be stacked
+        self.frames_stacked = deque([], maxlen=k)
+
+        self.frame_width = 84
+        self.frame_height = 84
+
+        logging.info(f"Image Observation is on")
+        super().__init__(config)
+
+    # @override
+    @property
+    def observation_space(self):
+        return (9, self.frame_width, self.frame_height)
+
+    # @override
+    def reset(self):
+        _ = self.env.reset()
+        frame = self.grab_frame(height=self.frame_height, width=self.frame_width)
+        frame = np.moveaxis(frame, -1, 0)
+        for _ in range(self.k):
+            self.frames_stacked.append(frame)
+        stacked_frames = np.concatenate(list(self.frames_stacked), axis=0)
+        return stacked_frames
+
+    # @override
+    def step(self, action):
+        _, reward, done, truncated, _ = self.env.step(action)
+        frame = self.grab_frame(height=self.frame_height, width=self.frame_width)
+        frame = np.moveaxis(frame, -1, 0)
+        self.frames_stacked.append(frame)
+        stacked_frames = np.concatenate(list(self.frames_stacked), axis=0)
+        return stacked_frames, reward, done, truncated
