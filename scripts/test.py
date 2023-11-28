06a81dca105d5ac9b4f761bdb799e6e2bcf7b5e9
beardyface <henryamwilliams@gmail.com>
2023-11-29 08:48:24 +1300

    pyboy game area prototype

--- scripts/test.py
+++ scripts/test.py
@@ -1,33 +1,38 @@
 import logging
 
 import cv2
+
+import pandas as pd
+
 import pandas as pd
 from envrionments.environment_factory import EnvironmentFactory
 from util.configurations import GymEnvironmentConfig
+from envrionments.pyboy.pokemon.Pokemon import Pokemon, PokemonImage
+from envrionments.pyboy.mario.Mario import Mario, MarioImage
 
-
-def key_to_action(button: int):
-    key_map = {
-        115: 0,  # s - down
-        97: 1,  # a - left
-        100: 2,  # d - right
-        119: 3,  # w - up
-        122: 4,  # z - A
-        120: 5,  # x - B
+def key_to_action(key):
+    map = {
+        115: 0, #s - down
+        97: 1,  #a - left
+        100: 2, #d - right
+        119: 3, #w - up
+        122: 4, #z - A
+        120: 5, #x - B
         # 32: 6,  #space - start
     }
-    logging.info(f"Key: {button}")
-    if button in key_map:
-        logging.info(f"Map: {key_map[button]}")
-        return key_map[button]
-    return -1
+    logging.info(f"Key: {key}")
+    if key in map.keys():
+        logging.info(f"Map: {map[key]}")
+        return map[key]
+    else:
+        return -1
 
 
 if __name__ == "__main__":
     args = {
-        "gym": "pyboy",
+        'gym' : 'pyboy',
         # 'task' : 'pokemon',
-        "task": "mario",
+        'task' : 'mario',
     }
     env_config = GymEnvironmentConfig(**args)
 
@@ -47,15 +52,15 @@ if __name__ == "__main__":
         if action == -1:
             break
 
-        # TODO - action needs to be made continuous
-        state, reward, done, _ = env.step(action)
+        state, reward, done, _  = env.step(action, discrete=True)
         image = env.grab_frame()
 
         stats = env._generate_game_stats()
+        # logging.info(stats)
 
         game_area = env.game_area()
 
-        area = pd.DataFrame(game_area)
+        area = pd.DataFrame(game_area)        
 
         print(area)
         logging.info(game_area.shape)
