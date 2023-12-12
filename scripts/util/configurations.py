fc5b7208bccc957bc60df6dab194fe915db46e45
Akeeshan-S <akeeshan.siv@gmail.com>
2023-12-13 10:40:16 +1300

    fix: merge errors

--- scripts/util/configurations.py
+++ scripts/util/configurations.py
@@ -1,37 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 """
 Configuration class for Gym Environments.
 """
 
 from pathlib import Path
 from typing import Optional
-=======
-import logging
-import json
-=======
-"""
-Configuration class for Gym Environments.
-"""
->>>>>>> 3457986 (Pylint corrections)
 
-from pathlib import Path
-from typing import Optional
-
-<<<<<<< HEAD
-file_path = Path(__file__).parent.resolve()
->>>>>>> d58b901 (type hinting + linting across the board)
-
-=======
->>>>>>> 3457986 (Pylint corrections)
 from pydantic import Field
 
 from cares_reinforcement_learning.util.configurations import EnvironmentConfig
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3457986 (Pylint corrections)
 file_path = Path(__file__).parent.resolve()
 
 
@@ -50,13 +27,6 @@ class GymEnvironmentConfig(EnvironmentConfig):
         headless (Optional[bool]): Whether to run in headless mode (default: False)
     """
 
-<<<<<<< HEAD
-=======
-
-class GymEnvironmentConfig(EnvironmentConfig):
->>>>>>> d58b901 (type hinting + linting across the board)
-=======
->>>>>>> 3457986 (Pylint corrections)
     gym: str = Field(description="Gym Environment <openai, dmcs, pyboy>")
     task: str
     domain: Optional[str] = ""
@@ -67,14 +37,6 @@ class GymEnvironmentConfig(EnvironmentConfig):
     emulation_speed: Optional[int] = 0
     headless: Optional[int] = 0
 
-<<<<<<< HEAD
-=======
-    rom_path: Optional[str] = f"{Path.home()}/cares_rl_configs"
-    act_freq: Optional[int] = 24
-    emulation_speed: Optional[int] = 0
-    headless: Optional[bool] = False
-
->>>>>>> d58b901 (type hinting + linting across the board)
 
 # class OpenAIEnvironmentConfig(GymEnvironmentConfig):
 #     gym: str = Field("openai", Literal=True)
