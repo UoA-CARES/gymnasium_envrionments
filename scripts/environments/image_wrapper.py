dbcaebd84368fe6cad6011aa6617b8515972988b
beardyface <henryamwilliams@gmail.com>
2025-04-17 16:13:33 +1200

    minor

--- scripts/environments/image_wrapper.py
+++ scripts/environments/image_wrapper.py
@@ -63,7 +63,7 @@ class ImageWrapper:
             frame.resize((height, width, 1))
         return frame
 
-    def reset(self) -> dict:
+    def reset(self) -> dict[str, np.ndarray]:
         vector_state = self.gym.reset()
         frame = self.grab_frame(
             height=self.frame_height, width=self.frame_width, grey_scale=self.grey_scale
