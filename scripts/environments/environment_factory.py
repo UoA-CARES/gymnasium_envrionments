4919a70fba96dbb1a99fa5e1e86620484f0ba5ff
beardyface <henryamwilliams@gmail.com>
2025-06-30 15:23:14 +1200

    basic example of pokeenv

--- scripts/environments/environment_factory.py
+++ scripts/environments/environment_factory.py
@@ -12,22 +12,35 @@ class EnvironmentFactory:
 
     def create_environment(
         self, config: GymEnvironmentConfig, image_observation
-    ) -> GymEnvironment | ImageWrapper:
+    ) -> tuple[GymEnvironment | ImageWrapper, GymEnvironment | ImageWrapper]:
 
         env: GymEnvironment | ImageWrapper
+        eval_env: GymEnvironment | ImageWrapper
         if config.gym == "dmcs":
             from environments.dmcs.dmcs_environment import DMCSEnvironment
 
             env = DMCSEnvironment(config)
+            eval_env = DMCSEnvironment(config)
         elif config.gym == "openai":
             from environments.openai.openai_environment import OpenAIEnvironment
 
             env = OpenAIEnvironment(config)
+            eval_env = OpenAIEnvironment(config)
         elif config.gym == "pyboy":
             from environments.pyboy.pyboy_environment import PyboyEnvironment
 
             env = PyboyEnvironment(config)
+            eval_env = PyboyEnvironment(config)
+        elif config.gym == "pokeenv":
+            from environments.pokeenv.pokeenv_environment import PokeEnvEnvironment
+
+            env = PokeEnvEnvironment(config, evaluation=False)
+            eval_env = PokeEnvEnvironment(config, evaluation=True)
         else:
             raise ValueError(f"Unkown environment: {config.gym}")
 
-        return ImageWrapper(config, env) if bool(image_observation) else env
+        env = ImageWrapper(config, env) if bool(image_observation) else env
+        eval_env = (
+            ImageWrapper(config, eval_env) if bool(image_observation) else eval_env
+        )
+        return env, eval_env
---
+++ scripts/environments/pokeenv/pokeenv_environment.py
@@ -0,0 +1,79 @@
+import time
+from functools import cached_property
+
+import cv2
+import numpy as np
+from gymnasium import spaces
+from poke_env import SimpleHeuristicsPlayer
+from poke_env.player.single_agent_wrapper import SingleAgentWrapper
+from poke_env_gym.poke_environment import PokeEnvironment
+from util.configurations import GymEnvironmentConfig
+
+from environments.gym_environment import GymEnvironment
+
+
+class PokeEnvEnvironment(GymEnvironment):
+    def __init__(self, config: GymEnvironmentConfig, evaluation: bool = False) -> None:
+        super().__init__(config)
+
+        account_name_one: str = "train_one" if not evaluation else "eval_one"
+        account_name_two: str = "train_two" if not evaluation else "eval_two"
+
+        self.primary_env = PokeEnvironment(
+            account_name_one=account_name_one, account_name_two=account_name_two
+        )
+
+        self.env = SingleAgentWrapper(self.primary_env, SimpleHeuristicsPlayer())
+
+        time.sleep(3)  # Allow the environment to initialize properly
+
+    @cached_property
+    def max_action_value(self) -> float:
+        return self.env.action_space.high[0]
+
+    @cached_property
+    def min_action_value(self) -> float:
+        return self.env.action_space.low[0]
+
+    @cached_property
+    def observation_space(self) -> int:
+        return self.env.observation_space.shape[0]
+
+    @cached_property
+    def action_num(self) -> int:
+        if isinstance(self.env.action_space, spaces.Box):
+            action_num = self.env.action_space.shape[0]
+        elif isinstance(self.env.action_space, spaces.Discrete):
+            action_num = self.env.action_space.n
+        else:
+            raise ValueError(
+                f"Unhandled action space type: {type(self.env.action_space)}"
+            )
+        return action_num
+
+    def sample_action(self) -> int:
+        return self.env.action_space.sample()
+
+    def set_seed(self, seed: int) -> None:
+        _, _ = self.env.reset(seed=seed)
+        # Note issues: https://github.com/rail-berkeley/softlearning/issues/75
+        self.env.action_space.seed(seed)
+
+    def reset(self, training: bool = True) -> np.ndarray:
+        state, _ = self.env.reset()
+        return state
+
+    def step(self, action: int) -> tuple:
+        state, reward, done, truncated, _ = self.env.step(np.int64(action))
+        return state, reward, done, truncated
+
+    def grab_frame(self, height: int = 240, width: int = 300) -> np.ndarray:
+        frame = self.env.render()
+        frame = cv2.resize(frame, (width, height))
+        # Convert to BGR for use with OpenCV
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        return frame
+
+    def get_overlay_info(self) -> dict:
+        # TODO: Add overlay information for gyms as needed
+        return {}
