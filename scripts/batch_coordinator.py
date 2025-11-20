6519801f0fe29628265ba50f60ee9604ccc70677
Oculux314 <nate.williamson.nz@gmail.com>
2025-11-21 12:11:14 +1300

    allow skipping certain batch configurations

--- scripts/batch_coordinator.py
+++ scripts/batch_coordinator.py
@@ -7,11 +7,11 @@ configuration combination and prepares them for execution.
 import itertools
 from typing import Any
 
-from cares_reinforcement_learning.util.configurations import FunctionLayer, MLPConfig, TrainableLayer
-
 from execution_coordinator import ExecutionCoordinator
 import execution_logger as logs
 from util.rl_parser import RLParser
+
+from cares_reinforcement_learning.util.configurations import FunctionLayer, MLPConfig, TrainableLayer
 
 relu: MLPConfig = MLPConfig(
         layers=[
@@ -48,9 +48,26 @@ golu: MLPConfig = MLPConfig(
 # to create multiple experiment configurations.
 batch_config: dict[str, list[Any | tuple[Any, str]]] = {
     "alg_config.network_config": [(relu, "relu"), (gelu, "gelu"), (golu, "golu")],
-    # "env_config.domain": ["ball_in_cup", "walker"],
-    # "env_config.task": ["walk", "catch"],
+    "env_config.domain": ["ball_in_cup", "walker"],
+    "env_config.task": ["catch", "walk"],
 }
+
+def _skip(config: dict[str, tuple[Any, str]]) -> bool:
+    """Determine if a given configuration combination should be skipped.
+    E.g., task walker.catch doesn't exist.
+
+    Args:
+        config (dict[str, tuple[Any, str]]): A configuration mapping where each
+            key is a configuration attribute path and each value is a tuple of
+            (actual_value, name).
+    Returns:
+        bool: True if the configuration should be skipped, False otherwise.
+    """
+    # Implement skipping logic here if needed
+    return (config.get("env_config.domain", (None,))[0] == "ball_in_cup" and config.get("env_config.task", (None,))[0] == "walk") \
+        or (config.get("env_config.domain", (None,))[0] == "walker" and config.get("env_config.task", (None,))[0] == "catch")
+
+# -------------------------------------------------------------------
 
 # Get the main logger for this function
 logger = logs.get_main_logger()
@@ -74,19 +91,23 @@ def get_batch_coordinators() -> list[tuple[ExecutionCoordinator, str]]:
     """
     # Expand batch configs into all combinations
     keys = list(batch_config.keys())
-    configs = [create_config(keys, config_values) for config_values in itertools.product(*batch_config.values())]
+    configs = [_create_config(keys, config_values) for config_values in itertools.product(*batch_config.values())]
 
     coordinators: list[tuple[ExecutionCoordinator, str]] = []
     for i, config in enumerate(configs):
+        # Certain combinations may be invalid - skip these
+        if _skip(config):
+            continue
+
         # Setup specific name and coordinator
-        run_name = get_name_from_config(config, i+1)
-        coordinator = config_to_coordinator(config)
-        replace_configurations(coordinator, config)
+        run_name = _get_name_from_config(config, i+1)
+        coordinator = _config_to_coordinator(config)
+        _replace_configurations(coordinator, config)
         coordinators.append((coordinator, run_name))
 
     return coordinators
 
-def create_config(keys: list[str], config_values: tuple[Any | tuple[Any, str], ...]) -> dict[str, tuple[Any, str]]:
+def _create_config(keys: list[str], config_values: tuple[Any | tuple[Any, str], ...]) -> dict[str, tuple[Any, str]]:
     config: dict[str, tuple[Any, str]] = {}
     for i, value in enumerate(config_values):
         # Ensure value is a tuple (actual_value, name)
@@ -96,20 +117,20 @@ def create_config(keys: list[str], config_values: tuple[Any | tuple[Any, str], .
             config[keys[i]] = (value, f"{keys[i]}-{value}")
     return config
 
-def get_name_from_config(config: dict[str, tuple[Any, str]], index: int) -> str:
+def _get_name_from_config(config: dict[str, tuple[Any, str]], index: int) -> str:
     name_parts = []
     for value in config.values():
         name_parts.append(value[1])
     return f"[{index}]_" + "_".join(name_parts)
 
-def config_to_coordinator(config: dict[str, tuple[Any, str]]) -> ExecutionCoordinator:
+def _config_to_coordinator(config: dict[str, tuple[Any, str]]) -> ExecutionCoordinator:
     parser = RLParser()
     base_configs = parser.parse_args()
     coordinator = ExecutionCoordinator(base_configs)
-    replace_configurations(coordinator, config)
+    _replace_configurations(coordinator, config)
     return coordinator
 
-def replace_configurations(coordinator: ExecutionCoordinator, config: dict[str, tuple[Any, str]]):
+def _replace_configurations(coordinator: ExecutionCoordinator, config: dict[str, tuple[Any, str]]):
     for key, value in config.items():
         keys = key.split('.')
         obj = coordinator
