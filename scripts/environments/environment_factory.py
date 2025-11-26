3a96ee362c4088055a53bd656462da8fe36c132e
beardyface <henryamwilliams@gmail.com>
2025-11-27 08:57:50 +1300

    revert odd changes

--- scripts/environments/environment_factory.py
+++ scripts/environments/environment_factory.py
@@ -41,7 +41,7 @@ class EnvironmentFactory:
             from environments.showdown.showdown_environment import ShowdownEnvironment
 
             env = ShowdownEnvironment(config, evaluation=False)
-            eval_env = ShowdownEnvironment(config, evaluation=True)
+            eval_env = env
 
         elif isinstance(config, cfg.DroneConfig):
             from environments.drone.drone_environment import DroneEnvironment
