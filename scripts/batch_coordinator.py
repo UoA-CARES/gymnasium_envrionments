31d96bfcab2fedfa33bcad98ae3c42c9e6c45e7f
Nate Williamson <nwil508@aucklanduni.ac.nz>
2026-01-09 10:53:10 +1300

    change scp path

--- scripts/batch_coordinator.py
+++ scripts/batch_coordinator.py
@@ -392,7 +392,7 @@ first_critic_golu_c: MLPConfig = MLPConfig(
 
 # cd /app/gymnasium_envrionments/scripts && nano batch_coordinator.py
 # bash ../strip_logs.sh /app/cares_rl_logs
-# scp -r /app/cares_rl_logs/TD3 anyone@130.216.238.228:/home/anyone/Documents/nwil508/logs/saved/gelu-GoLU
+# scp -r /app/cares_rl_logs/TD3 anyone@130.216.238.228:/home/anyone/Documents/nwil508/logs/tmp/R8
 # sed -i 's/_td3,/_sac,/g' ./batch_coordinator.py && python3 run.py train cli --gym dmcs --domain cartpole --task swingup --batch 1 SAC --seeds 10 20 30 40 50 --max_workers 5
 # sed -i 's/_td3,/_sac,/g' ./batch_coordinator.py && python3 run.py train cli --gym openai --task HalfCheetah-v4 --batch 1 SAC --seeds 10 20 30 40 50 --max_workers 5
 
